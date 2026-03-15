e874b6a6db672b99cc1ceaffcd4345759082ab3d
unknown <you@example.com>
2026-03-15 11:06:46 +0800

    Fix Dark Mode in Production: Configure Tailwind CDN to use 'class' strategy

--- resources/views/layouts/mobile.blade.php
+++ resources/views/layouts/mobile.blade.php
@@ -23,6 +23,11 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
   <script src="https://cdn.tailwindcss.com"></script>
+  <script>
+    tailwind.config = {
+      darkMode: 'class',
+    }
+  </script>
   @vite(['resources/css/mobile.css', 'resources/js/mobile.js'])
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
   
